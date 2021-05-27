c5cf67600b0a2800af25efe8cb67910b329c9ce2
Benoit Bénézech <benoit.benezech@gmail.com>
2021-05-27 15:49:59 +0200

    More doc

--- src/Other.jsx
+++ src/Other.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
 // break me! (change <span> to <spa> for ex.)
+// then try to revert => webpack won't peak up changes
 const Other = ({ children }) => <span>{children}</span>;
 export default Other;
