145f4dfdb2cac094093f22c16ca6e724be3244c2
Benoit Bénézech <benoit.benezech@gmail.com>
2021-05-27 15:51:47 +0200

    More doc

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Other from "./Other";
 
+// if I am inlined, no pb. (uncomment)
+// const Other = ({ children }) => <span>{children}</span>;
+
+// if I am not `styled`, no pb (comment)
 const StyledOther = styled(Other)``;
+
 ReactDOM.render(
   <StyledOther>Hello</StyledOther>,
   window.document.body
